5ef3ff661de7d6cc8a3513d655d5ae8de52c4cd6
Elena Stepanova <lady.alena-he2015@yandex.ru>
2022-07-07 14:34:15 +0300

    Changed task 2

--- 02/main.cpp
+++ 02/main.cpp
@@ -1,74 +1,47 @@
 ﻿#include <iostream>
 #include <string>
 
+
 class Counter {
 private:
 	int i;
 
 public:
 
-	int work_i;
-
 	Counter(int i) {
 		this->i = i;
-		this->work_i = i;
 	}
 	Counter() {
 		i = 1;
-		work_i = i;
 	}
 
-	void increase(int &i) {
-		this->i = ++i;
+	int increase() {
+		return ++i;
 	}
 
-	void reduce(int &i) {
-		this->i = --i;
+	int reduce() {
+		return --i;
 	}
 
-	void show_counter(int i) {
-		this->i = i;
-		std::cout << this->i << std::endl;
+	void show_counter() {
+		std::cout << i << std::endl;
 	}
 };
 
-int main(int argc, char** argv) {
-
-	system("chcp 1251");
-
-	Counter count;
-
-	std::string answer;
-
-	do {
-		std::cout << "Вы хотите указать начальное значение счетчика? Введите да или нет: ";
-		(std::cin >> answer).get();
-
-		if (answer != "да" && answer != "нет") {
-			std::cout << "Некорректное сообщение!" << std::endl;
-		}
-
-	} while (answer != "да" && answer != "нет");
-
-
-	if (answer == "да") {
-		std::cout << "Введите начальное значение счетчика: ";
-		std::cin >> count.work_i;
-	}
-
+void Options(Counter counter_example, std::string answer) {
 	do {
 		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
-		
-		(std::cin >> answer).get();
-		
+
+		std::getline(std::cin, answer);
+
 		if (answer == "+") {
-			count.increase(count.work_i);
+			counter_example.increase();
 		}
 		else if (answer == "-") {
-			count.reduce(count.work_i);
+			counter_example.reduce();
 		}
 		else if (answer == "=") {
-			count.show_counter(count.work_i);
+			counter_example.show_counter();
 		}
 		else if (answer == "x") {
 			std::cout << "До свидания!" << std::endl;
@@ -79,6 +52,38 @@ int main(int argc, char** argv) {
 		}
 
 	} while (!(answer == "x"));
+}
+
+int main(int argc, char** argv) {
+
+	system("chcp 1251");
+
+	std::string answer;
+
+	do {
+		std::cout << "Вы хотите указать начальное значение счетчика? Введите да или нет: ";
+
+		std::getline(std::cin, answer);
+
+		if (answer != "да" && answer != "нет") {
+			std::cout << "Некорректное сообщение!" << std::endl;
+		}
+
+	} while (answer != "да" && answer != "нет");
+
+
+	if (answer == "да") {
+		int i = 0;
+		std::cout << "Введите начальное значение счетчика: ";
+		std::cin >> i;
+		while (std::cin.get() != '\n') continue;
+		Counter count_with_initial_value(i);
+		Options(count_with_initial_value, answer);
+	}
+	else {
+		Counter count;
+		Options(count, answer);
+	}
 
 	return 0;
 }
